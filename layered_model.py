bae79e2155c02ddc3dd4c422d5278b3271ca9745
Ibrahim Maïga <64782594+Ibrahim-Maiga@users.noreply.github.com>
2024-05-28 20:04:51 -0400

    Update layered_model.py

--- layered_model.py
+++ layered_model.py
@@ -8,7 +8,11 @@ def define_dense_model_single_layer(input_length, activation_f='sigmoid', output
     input_length: the number of inputs
     activation_f: the activation function
     output_length: the number of outputs (number of neurons)"""
-    model = None
+    model = keras.Sequential([
+        layers.Input(shape=(input_length,)),
+        layers.Dense(output_length, activation=activation_f)
+    ])
+    return model
 
 def define_dense_model_with_hidden_layer(input_length, 
                                          activation_func_array=['relu','sigmoid'],
@@ -19,10 +23,12 @@ def define_dense_model_with_hidden_layer(input_length,
     activation_func_array: the activation function for the hidden layer and the output layer
     hidden_layer_size: the number of neurons in the hidden layer
     output_length: the number of outputs (number of neurons in the output layer)"""
-
-    model = None
+    model = keras.Sequential([
+        layers.Input(shape=(input_length,)),
+        layers.Dense(hidden_layer_size, activation=activation_func_array[0]),
+        layers.Dense(output_length, activation=activation_func_array[1])
+    ])
     return model
-
 
 def get_mnist_data():
     """Get the MNIST data."""
@@ -33,7 +39,7 @@ def get_mnist_data():
 
 def binarize_labels(labels, target_digit=2):
     """Binarize the labels."""
-    labels = 1*(labels==target_digit)
+    labels = 1 * (labels == target_digit)
     return labels
 
 def fit_mnist_model_single_digit(x_train, y_train, target_digit, model, epochs=10, batch_size=128):
@@ -44,6 +50,8 @@ def fit_mnist_model_single_digit(x_train, y_train, target_digit, model, epochs=1
     then fit the model on the training data. (pass the epochs and batch_size params)
     """
     y_train = binarize_labels(y_train, target_digit)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
     return model
 
 def evaluate_mnist_model_single_digit(x_test, y_test, target_digit, model):
@@ -51,5 +59,5 @@ def evaluate_mnist_model_single_digit(x_test, y_test, target_digit, model):
     Hint: use model.evaluate() to evaluate the model on the test data.
     """
     y_test = binarize_labels(y_test, target_digit)
-    loss, accuracy = None, None  ## change this.
+    loss, accuracy = model.evaluate(x_test, y_test)
     return loss, accuracy
